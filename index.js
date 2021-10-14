e77109a78a246d512b4771fe36f74071a257f398
Wes Schierenbeck <wschierenbeck@gmail.com>
2021-10-14 09:57:17 -0400

    commit tree 39af44f1b19df32d9681c42105357c5b766d6113

--- index.js
+++ index.js
@@ -1,7 +1,60 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
+
 function superbowlWin(array){
+    const winningYear = array.find(array => array.result==="W")
+    if (winningYear){
+        return winningYear.year
+    } else {
+        return undefined
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function superbowlWinForOf(array){
     for (const objects of array){
         if (objects.result==="W"){
             return objects.year
         }
     }
 }
+
+
+const nbaTeams=[
+    {city: "New York",
+    team: "Knicks",},
+    {city: "Los Angeles",
+    team: "Lakers,"},
+    {city: "Memphis",
+    team: "Grizzlies",}
+]
+
